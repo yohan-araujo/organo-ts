0336106751b178d07e1ba69ea89a120a60af39b5
Yohan Araujo <yohanneves@outlook.pt>
2023-04-06 21:06:08 -0300

    Projeto utilizando TypeScript pela primeira vez

---
+++ src/componentes/CampoTexto/index.tsx
@@ -0,0 +1,40 @@
+import "./CampoTexto.css";
+
+interface CampoTextoProps {
+  aoAlterado: (valor: string) => void;
+  placeholder: string;
+  label: string;
+  valor: string;
+  obrigatorio?: boolean;
+  tipo?: "text" | "password" | "date" | "email" | "number";
+}
+
+const CampoTexto = ({
+  aoAlterado,
+  label,
+  placeholder,
+  valor,
+  tipo = "text",
+  obrigatorio = false,
+}: CampoTextoProps) => {
+  const placeholderModificada = `${placeholder}...`;
+
+  const aoDigitado = (evento: React.ChangeEvent<HTMLInputElement>) => {
+    aoAlterado(evento.target.value);
+  };
+
+  return (
+    <div className="campo-texto">
+      <label>{label}</label>
+      <input
+        type={tipo}
+        value={valor}
+        onChange={aoDigitado}
+        required={obrigatorio}
+        placeholder={placeholderModificada}
+      />
+    </div>
+  );
+};
+
+export default CampoTexto;
